2541844697636a05a026943d2c65d2e447f6afc4
yegorf1 <yegorf1@gmail.com>
2018-08-27 00:34:52 +0300

    Simple implementation done

--- main.py
+++ main.py
@@ -4,26 +4,51 @@ from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
+
+from translate_shell_parser import TranslateShellParser
 
 
-class DemoExtension(Extension):
-
+class TranslateExtension(Extension):
     def __init__(self):
-        super(DemoExtension, self).__init__()
+        super(TranslateExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
 
 
 class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
-        items = []
-        for i in range(5):
-            items.append(ExtensionResultItem(icon='images/icon.png',
-                                             name='Item %s' % i,
-                                             description='Item description %s' % i,
-                                             on_enter=HideWindowAction()))
+        query = event.get_argument() or str()
+
+        if len(query.strip()) == 0:
+            return RenderResultListAction([
+                ExtensionResultItem(icon='images/gt-icon.png',
+                                    name='No input',
+                                    on_enter=HideWindowAction())
+            ])
+
+        try:
+            translation = TranslateShellParser(query).execute()
+        except OSError:
+            return RenderResultListAction([
+                ExtensionResultItem(
+                    icon='images/gt-icon.png',
+                    name='Looks like you have no translate-shell installed',
+                    description="Select to open repo",
+                    on_enter=OpenUrlAction('https://github.com/soimort/translate-shell')
+                )
+            ])
+
+        items = [
+            ExtensionResultItem(icon='images/gt-icon.png',
+                                name=translation.translation,
+                                description=translation.part_of_speech + ', ' + ' '.join(translation.synonyms),
+                                on_enter=HideWindowAction())
+
+            for translation in translations
+        ]
 
         return RenderResultListAction(items)
 
 
 if __name__ == '__main__':
-    DemoExtension().run()
+    TranslateExtension().run()
